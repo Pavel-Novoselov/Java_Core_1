0c950b16b807a5afcd0af55c7375e937cf7d25d7
Pavel-Novoselov <npe@inbox.ru>
2019-09-19 22:54:42 +0300

    факультатив зад. доделано

--- src/facultative/DWeek.java
+++ src/facultative/DWeek.java
@@ -1,4 +1,22 @@
 package facultative;
 
-public class DWeek {
+public enum DWeek{
+    MONDAY (40,1), TUESDAY(32,2), WEDNESDAY(24,3), THURSDAY(16,4), FRIDAY(8,5), SATURDAY(0,6), SONDAY(0,7);
+
+    private int hours; //hours till the end of working week
+    private int dNum; //the number of week-day
+
+    DWeek(int hours, int dNum) {
+        this.hours = hours;
+        this.dNum = dNum;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getdNum() {
+        return dNum;
+    }
+
 }
--- src/facultative/DayOfWeek.java
+++ src/facultative/DayOfWeek.java
@@ -3,43 +3,9 @@ package facultative;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-enum DWeek{
-    MONDAY (40,1), TUESDAY(32,2), WEDNESDAY(24,3), THURSDAY(16,4), FRIDAY(8,5), SATURDAY(0,6), SONDAY(0,7);
-
-    private int hours; //hours till the end of working week
-    private int dNum; //the number of week-day
-
-    DWeek(int hours, int dNum) {
-        this.hours = hours;
-        this.dNum = dNum;
-    }
-
-    public int getHours() {
-        return hours;
-    }
-
-    public int getdNum() {
-        return dNum;
-    }
-
-}
-
 public class DayOfWeek {
     public static void main(String[] args) {
-        Calendar newCal = new GregorianCalendar();
-        int day = newCal.get( Calendar.DAY_OF_WEEK );
-        //System.out.println(day);
-
-        for (DWeek d: DWeek.values()){
-          //  System.out.println(d.getHours());
-            if (d.getdNum()==day && d.getHours()==0){
-                System.out.println("Today is a weekend!");
-            }
-            else if (d.getdNum()==day && d.getHours()!=0){
-                System.out.println(d.getHours()+" hours left till the end of the working week");
-            }
-        }
-
-
+        HoursCalc hc = new HoursCalc();
+        hc.calcHours();
     }
 }
